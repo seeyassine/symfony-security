fc62d90705df775f87ce9966e1984163daf439c6
See Yassine <seeyassine@gmail.com>
2025-11-23 16:46:59 +0100

    add my own Queries builderfindOneByName

--- src/Repository/LocationRepository.php
+++ src/Repository/LocationRepository.php
@@ -34,6 +34,24 @@ class LocationRepository extends ServiceEntityRepository
             $em->flush();
         }
     }
+
+
+    public function findOneByName(string $name): ?Location
+    {
+        $qb = $this->createQueryBuilder('l');
+        $qb
+            ->where('l.name =  :name')
+            ->setParameter('name', $name)
+            // ->andWhere('l.countryCode = :countryCode')
+            // ->setParameter('countryCode', 'ES')
+        ;
+
+        $query = $qb->getQuery();
+        $entity = $query->getOneOrNullResult();
+
+        return $entity;
+    }
+
 //    /**
 //     * @return Location[] Returns an array of Location objects
 //     */
